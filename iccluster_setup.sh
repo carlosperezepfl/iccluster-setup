1c80405c1817a75b04c6dd2766945337d333a226
Carlos Perez <carlos.perez@epfl.ch>
2018-10-15 09:47:57 +0200

    Update iccluster_setup.sh

--- iccluster_setup.sh
+++ iccluster_setup.sh
@@ -2,42 +2,42 @@
 
 # update repositories and upgrade packages
 apt-get update
-apt-get --assume-yes upgrade
-apt-get update
-apt-get --assume-yes upgrade
+apt-get upgrade -y
 
+#MySQL ask for password 
+export DEBIAN_FRONTEND=noninteractive
+# or provide a fake password
+#sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password your_password'
+#sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password your_password'
 # install python and tools
-apt-get --assume-yes install python3 python3-dev python3-pip python3-yaml git nano screen wget zip unzip g++ htop software-properties-common pkg-config zlib1g-dev gdb cmake cmake-curses-gui autoconf gcc gcc-multilib g++-multilib mysql-server 
-
-mkdir /home/downloads
-cd /home/downloads
+apt-get install python3 python3-dev python3-pip python3-yaml git nano screen wget zip unzip g++ htop software-properties-common pkg-config zlib1g-dev gdb cmake cmake-curses-gui autoconf gcc gcc-multilib g++-multilib mysql-server -y
 
 # download and install CUDA
 VERSION="10.0"
 SUB_VERSION="130"
 SUB_SUB_VERSION="1"
 CUDA_TAR_FILE="cuda-${VERSION}.${SUB_VERSION}-${SUB_SUB_VERSION}.deb"
-wget http://lia.epfl.ch/dependencies/${CUDA_TAR_FILE}
+wget http://lia.epfl.ch/dependencies/${CUDA_TAR_FILE} -O /tmp/${CUDA_TAR_FILE}
 apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
-dpkg -i ./${CUDA_TAR_FILE}
+dpkg -i /tmp/${CUDA_TAR_FILE}
 apt-get update
-apt-get --assume-yes install cuda=${VERSION}.${SUB_VERSION}-${SUB_SUB_VERSION}
+apt-get install cuda=${VERSION}.${SUB_VERSION}-${SUB_SUB_VERSION} -y
 
 # download and install libcudnn
 CUDNN_VERSION="7.3"
 CUDNN_TAR_FILE="cudnn-${VERSION}-${CUDNN_VERSION}.tgz"
-wget http://lia.epfl.ch/dependencies/${CUDNN_TAR_FILE}
-tar -xzvf ${CUDNN_TAR_FILE}
-mkdir /usr/local/cuda-${VERSION}
-mkdir /usr/local/cuda-${VERSION}/lib64
+wget http://lia.epfl.ch/dependencies/${CUDNN_TAR_FILE} -O /tmp/${CUDNN_TAR_FILE}
+tar -xzvf /tmp/${CUDNN_TAR_FILE}  -C /tmp/
+#mkdir /usr/local/cuda-${VERSION}
+mkdir -p /usr/local/cuda-${VERSION}/lib64
 
-cp -P cuda/include/cudnn.h /usr/local/cuda-${VERSION}/include
-cp -P cuda/lib64/libcudnn* /usr/local/cuda-${VERSION}/lib64/
+cp -P /tmp/cuda/include/cudnn.h /usr/local/cuda-${VERSION}/include
+cp -P /tmp/cuda/lib64/libcudnn* /usr/local/cuda-${VERSION}/lib64/
 chmod a+r /usr/local/cuda-${VERSION}/lib64/libcudnn*
 
 # install python packages for machine learning
-yes | pip3 install --upgrade pip
-yes | pip3 install pillow matplotlib mpmath jupyter pandas sklearn tensorflow spacy dill numpy configparser gensim pymysql stanford-corenlp cython networkx bs4 mako fuzzywuzzy langdetect python-levenshtein pyldavis newspaper3k wikipedia nltk py-rouge pyrouge beautifultable tensor2tensor tensorboardX
+/usr/bin/yes | pip3 install --upgrade pip
+/usr/bin/yes | pip3 install pillow matplotlib mpmath jupyter pandas sklearn tensorflow spacy dill numpy configparser gensim pymysql stanford-corenlp cython networkx bs4 mako fuzzywuzzy langdetect python-levenshtein pyldavis newspaper3k wikipedia nltk py-rouge pyrouge beautifultable tensor2tensor tensorboardX
 pip3 install https://github.com/explosion/spacy-models/releases/download/en_core_web_lg-2.0.0/en_core_web_lg-2.0.0.tar.gz
 python3 -c "import nltk; nltk.download('punkt')"
 
